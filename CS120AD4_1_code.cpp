64b0a83a670981aaa4c22f698d9b3aef6f61612c
DAP6512 <rru002@gmail.com>
2024-09-25 11:11:29 -0700

    Creation of repository, renaming of local file names, and completion of if else png.

---
+++ CS120AD4_1_code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//main.exe
+    int main() 
+        {
+            //Steps:
+                //1. Variables:
+                    f
+                //2. User input:
+                    f
+                //3. If statements:
+                    if
+        }
